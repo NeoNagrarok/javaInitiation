9a482e611fc30b84de9582ce21812859839fc45d
Rémi <remi@signalwow.fr>
2021-01-06 11:12:51 +0100

    Interface SejourInterface created and implemented by class Sejour

--- src/main/java/reservations/Sejour.java
+++ src/main/java/reservations/Sejour.java
@@ -3,7 +3,7 @@ package reservations;
 import logements.Logement;
 import outils.MyDate;
 
-public class Sejour
+public class Sejour implements SejourInterface
 {
 	MyDate dateArrivee;
 	int nbNuits;
@@ -29,5 +29,33 @@ public class Sejour
 			" nuits."
 		);
 		System.out.println("Le prix de ce séjour est de " + (this.nbNuits * logement.tarifParNuit) + "€.");
+	}
+
+	/**
+	 * Verify if start date is valid.
+	 * Start date must be greater or egual than today plus 24h to be valid
+	 * 24 hours, 60 minutes, 60 secondes, 60 millisecondes are used for the comparison
+	 */
+	@Override
+	public boolean verificationDateArrivee() {
+		return this.dateArrivee.getTime() >= System.currentTimeMillis() + 24 * 60 * 60 * 1000;
+	}
+
+	/**
+	 * Verify if night number is between 1 included and 1 excluded.
+	 * For 31 days, there is only 30 nights.
+	 */
+	@Override
+	public boolean verificationNombreDenuits() {
+		return this.nbNuits >= 1 && this.nbNuits < 31;
+	}
+
+	/**
+	 * Verify if the trvelers number is valid.
+	 */
+	@Override
+	public boolean verificationNombreDeVoyageurs() {
+		// TODO Auto-generated method stub
+		return this.nbVoyageurs <= this.logement.getNbVoyageurs();
 	}
 }
---
+++ src/main/java/reservations/SejourInterface.java
@@ -0,0 +1,9 @@
+package reservations;
+
+public interface SejourInterface
+{
+	public boolean verificationDateArrivee();
+	public boolean verificationNombreDenuits();
+	public boolean verificationNombreDeVoyageurs();
+	public void afficher();
+}
